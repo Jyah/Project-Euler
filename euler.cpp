fec2e17a2903248c25c6f4889ee491f407cee15f
Jyah <j.dong@utah.edu>
2020-01-21 16:02:12 -0700

    p3: largest prime factor

--- euler.cpp
+++ euler.cpp
@@ -1,19 +1,52 @@
-//P2: Find the sum of all the even-valued terms in the Fibonacci sequence which do not exceed four million.
+//P3: Largest prime factor
 
 #include <iostream>
 #include <fstream>
+#include <stdlib.h> //atoi
+#include <stdio.h>
+#include <cstdint>
 using namespace std;
 
-int main() {
-	int limit = 4e6;
-	int sum = 0;
-	int a0 = 1, a1 = 1,a2=a0+a1;
-	while (a2 < limit) 
-	{
-		sum += a2;
-		a0 = a1 + a2;
-		a1 = a2 + a0;
-		a2 = a0 + a1;
+int main(int argc, char * argv[]) {
+	unsigned long long int n = atoll(argv[1]);
+	int factor = 2;
+	int lastFactor = 1;
+	while (n > 1) {
+		if (n % factor == 0) {
+			lastFactor = factor;
+			n /= factor;
+			while (n % factor == 0) {
+				n /= factor;
+			}
+		}
+		factor++;
 	}
-	cout << sum << '\n';
+	cout << lastFactor << '\n';
+	return 0;
 }
+
+//int main() {
+//	unsigned long long int n = 600851475143;
+//	int lastFactor = 0;
+//	if (n % 2 == 0) {
+//		n /= 2;
+//		 lastFactor = 2;
+//		while (n%2==0) {
+//			n /= 2;
+//		}
+//	}
+//	else{lastFactor = 1;}
+//	int factor = 3;
+//	while (n > 1) {
+//		if (n % factor == 0) {
+//			n /= factor;
+//			 lastFactor = factor;
+//			while (n%factor==0)
+//			{
+//				n /= factor;
+//			}
+//		}
+//		factor = factor + 2;
+//	}
+//	cout << lastFactor << '\n';
+//}
