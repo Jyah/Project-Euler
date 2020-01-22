3b6f3c5dcdd88d775d47d603930c54bf78daa23d
Jyah <j.dong@utah.edu>
2020-01-21 22:27:35 -0700

    p4: largest palindrome product

--- euler.cpp
+++ euler.cpp
@@ -1,4 +1,4 @@
-//P3: Largest prime factor
+//P4: Largest palindrome product
 
 #include <iostream>
 #include <fstream>
@@ -6,47 +6,42 @@
 #include <stdio.h>
 #include <cstdint>
 using namespace std;
-
-int main(int argc, char * argv[]) {
-	unsigned long long int n = atoll(argv[1]);
-	int factor = 2;
-	int lastFactor = 1;
-	while (n > 1) {
-		if (n % factor == 0) {
-			lastFactor = factor;
-			n /= factor;
-			while (n % factor == 0) {
-				n /= factor;
-			}
-		}
-		factor++;
+int reverse(int n)
+{
+	int reversed = 0;
+	while (n > 0) {
+		reversed = 10 * reversed + n % 10;
+		n /= 10;
 	}
-	cout << lastFactor << '\n';
+	return reversed;
+}
+bool isPalindrome(int n) {
+	if (n == reverse(n)) {
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int main() {
+	int largestPalindrome = 0;
+	int a = 999;
+	while (a >= 100) {
+		int b = 999;
+		while (b >=a) {
+			if (a*b<=largestPalindrome) {
+				break;
+			}
+			if (isPalindrome(a * b)) {
+				largestPalindrome = a * b;
+			}
+			b--;
+		}
+		a--;
+	}
+	cout << largestPalindrome << '\n';
 	return 0;
 }
 
-//int main() {
-//	unsigned long long int n = 600851475143;
-//	int lastFactor = 0;
-//	if (n % 2 == 0) {
-//		n /= 2;
-//		 lastFactor = 2;
-//		while (n%2==0) {
-//			n /= 2;
-//		}
-//	}
-//	else{lastFactor = 1;}
-//	int factor = 3;
-//	while (n > 1) {
-//		if (n % factor == 0) {
-//			n /= factor;
-//			 lastFactor = factor;
-//			while (n%factor==0)
-//			{
-//				n /= factor;
-//			}
-//		}
-//		factor = factor + 2;
-//	}
-//	cout << lastFactor << '\n';
-//}
