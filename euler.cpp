5d9b505abfd5d0cb6f31c8ac191510633179bfd4
Jyahway Dong <aca.27.dong@gmail.com>
2020-01-23 12:25:12 -0700

    P6:sum square difference

--- euler.cpp
+++ euler.cpp
@@ -1,55 +1,16 @@
-//P5: Smallest Multiples
+//P6:Sum square difference
 
 #include <iostream>
 #include <math.h>
-#include <vector>
+
 using namespace std;
 
-bool isPrime(int n)
-{
-	bool check = true;
-	for (int i = 2; i <= sqrt(n); i++) {
-		if (n % i == 0) {
-			check = false;
-			break;
-		}
-	}
-	return check;
-}
-
-vector<int> primes(int n) {
-	vector<int> prime_v;
-	int i = 0,j=2;
-	while (i < n) {
-		if (isPrime(j)) {
-			prime_v.push_back(j);
-			i++;
-		}
-		j++;
-	}
-	return prime_v;
-}
-
 int main(int argc, char *argv[]) {
-	int k = atoi(argv[1]);
-	unsigned long int  N = 1, i = 0;
-	vector<int> p = primes(100);
-	bool check = true;
-	int limit = sqrt(k);
-	vector<int> a;
-
-	while (p[i] <= k) {
-		a.push_back(1);
-		if (check) {
-			if (p[i] <= limit) {
-				a.back() = floor(log(k) / log(p[i]));
-			}
-			else {check = false;}
-		}
-		N = N * pow(p[i],a.back());// not ^, ^ is XOR in c++
-		i++;
-	}
-	cout << N << '\n';
+	int n = atoi(argv[1]);
+    int sum_sq = n*(2*n+1)*(n+1)/6;
+    int sum = n*(n+1)/2;
+    int dif = sum*sum-sum_sq;
+    cout<<dif<<'\n';
 	return 0;
 }
 
