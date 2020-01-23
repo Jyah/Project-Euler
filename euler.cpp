3f12c4c84b125a40db8e3ab3b15bfa6ee7723bc9
Jyahway Dong <aca.27.dong@gmail.com>
2020-01-23 13:00:12 -0700

    P6:10001st prime number

--- euler.cpp
+++ euler.cpp
@@ -1,16 +1,39 @@
-//P6:Sum square difference
+//P7:10001st prime
 
 #include <iostream>
 #include <math.h>
-
 using namespace std;
 
-int main(int argc, char *argv[]) {
-	int n = atoi(argv[1]);
-    int sum_sq = n*(2*n+1)*(n+1)/6;
-    int sum = n*(n+1)/2;
-    int dif = sum*sum-sum_sq;
-    cout<<dif<<'\n';
+bool isPrime(int n){
+    if(n==1){
+        return false;
+    }
+    else if(n<4){return true;}
+    else if(n%2==0){return false;}
+    else if(n<9){return true;}
+    else if(n%3==0){return false;}
+    else{
+        int r=floor(sqrt(n));
+        int f=5;
+        while(f<=r){
+            if(n%f==0){return false; break;}
+            if(n%(f+2)==0){return false; break;}
+            f+=6;
+        }
+        return true;
+    }
+}
+int main() {
+    int limit = 10001;
+    int count = 1;
+    int candidate = 1;
+    do {
+        candidate+=2;
+        if(isPrime(candidate)){
+            count++;
+        }
+    } while (count<limit);
+    cout<<candidate<<'\n';
 	return 0;
 }
 
